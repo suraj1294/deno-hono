75e62e91eeeadbaabea08709a430abd0388206ad
suraj <suraj@funnelstory.io>
2024-10-14 11:12:18 +0530

    add deno docker file

--- hono.ts
+++ hono.ts
@@ -4,4 +4,6 @@ const app = new Hono();
 
 app.get("/", (c) => c.text("Hello Deno!"));
 
-Deno.serve(app.fetch);
+//Deno.serve(app.fetch);
+
+Deno.serve({ port: +(Deno.env.get("PORT") || "3000") }, app.fetch);
