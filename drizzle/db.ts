90c2e616d723598f81a77953a51e2086ca14d8d7
suraj <suraj@funnelstory.io>
2024-10-14 18:34:05 +0530

    remove pg url

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -2,9 +2,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema.ts";
 
-const connection =
-  "postgresql://neondb_owner:xTbekB2EDf6M@ep-young-hat-a5d5jdn0.us-east-2.aws.neon.tech/neondb";
-
 export const connectionString = Deno.env.get("DATABASE_URL") || "";
 export const client = neon(connectionString);
 export const db = drizzle(client, { schema });
